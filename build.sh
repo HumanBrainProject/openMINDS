5045a7d7f9bb00caefadd528c9a5adbd9619d746
Oliver Schmid <oliver.schmid@ebrains.eu>
2021-06-29 23:21:30 +0200

    provide version information for generator

--- build.sh
+++ build.sh
@@ -28,51 +28,51 @@ build(){
   echo "Building version $1 (out of $2)"
   echo "*************************"
   echo ""
-#  git checkout $1
-#  # Ensure submodules are properly fetched
-#  git pull
-#  # Make sure we're at the head of the branch
-#  git reset --hard origin/$1
-#  git submodule sync
-#  git submodule update --init --recursive --remote
-#  # We push the synchronized state of the repository
-#  commitAndPush
-#
-#  #Use the vocab from the central repository - we remove an existing one (although there should be none)
-#  rm -rf vocab
-#  cp -r ../vocab .
-#
-#  # Linting...
-#  # stop the build if there are Python syntax errors or undefined names
-#  flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-#  # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-#  flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
-#
-#  # Run the generator logic
-#  if [[ $FIRST_BUILD == 1 ]]
-#    then
-#      echo "First build"
-#      python ../openMINDS_generator/openMINDS.py --path ../openMINDS --reinit --currentVersion "$1" --allVersions "$2"
-#    else
-#      python ../openMINDS_generator/openMINDS.py --path ../openMINDS --currentVersion "$1" --allVersions "$2"
-#  fi
-#  FIRST_BUILD=0
-#  # Copy expanded schemas into target
-#  echo "Copy expanded schemas into target"
-#  mkdir target/schema.tpl.json
-#  cp -r expanded/* target/schema.tpl.json
-#
-#  # Copy documentation
-#  rm -rf ../openMINDS_documentation/$1
-#  mkdir -p ../openMINDS_documentation/$1
-#
-#  # ZIP data
-#  cd target && zip -r ../../openMINDS_documentation/$1 . && cd ..
-#  cp -r target/html/* ../openMINDS_documentation/$1
-#  cp -r target/uml/* ../openMINDS_documentation/$1
-#  cp -r target/schema.json/* ../openMINDS_documentation/$1
-#
-#   cp -r vocab ..
+  git checkout $1
+  # Ensure submodules are properly fetched
+  git pull
+  # Make sure we're at the head of the branch
+  git reset --hard origin/$1
+  git submodule sync
+  git submodule update --init --recursive --remote
+  # We push the synchronized state of the repository
+  commitAndPush
+
+  #Use the vocab from the central repository - we remove an existing one (although there should be none)
+  rm -rf vocab
+  cp -r ../vocab .
+
+  # Linting...
+  # stop the build if there are Python syntax errors or undefined names
+  flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+  # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+  flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+
+  # Run the generator logic
+  if [[ $FIRST_BUILD == 1 ]]
+    then
+      echo "First build"
+      python ../openMINDS_generator/openMINDS.py --path ../openMINDS --reinit --currentVersion "$1" --allVersions "$2"
+    else
+      python ../openMINDS_generator/openMINDS.py --path ../openMINDS --currentVersion "$1" --allVersions "$2"
+  fi
+  FIRST_BUILD=0
+  # Copy expanded schemas into target
+  echo "Copy expanded schemas into target"
+  mkdir target/schema.tpl.json
+  cp -r expanded/* target/schema.tpl.json
+
+  # Copy documentation
+  rm -rf ../openMINDS_documentation/$1
+  mkdir -p ../openMINDS_documentation/$1
+
+  # ZIP data
+  cd target && zip -r ../../openMINDS_documentation/$1 . && cd ..
+  cp -r target/html/* ../openMINDS_documentation/$1
+  cp -r target/uml/* ../openMINDS_documentation/$1
+  cp -r target/schema.json/* ../openMINDS_documentation/$1
+
+   cp -r vocab ..
 }
 
 echo "Clearing existing elements..."
